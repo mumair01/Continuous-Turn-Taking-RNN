9e292221e4ceef5ec58e760fe112948a9f11f298
Muhammad Umair <umair.aarij@gmail.com>
2022-08-26 19:07:20 -0400

    Added initial scripts

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Muhammad Umair
+# @Date:   2022-08-26 17:53:15
+# @Last Modified by:   Muhammad Umair
+# @Last Modified time: 2022-08-26 17:54:11
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
